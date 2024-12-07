bfa40b1b9d7d8d454d8ef95d8c4c1675d5a8b281
SBerger_DEV <teebeutel.de@gmail.com>
2024-12-07 07:56:24 +0100

    add test.dart with comments

---
+++ test.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+
+
+void main () {
+print("test\ntest2\ntest3\ttest4"); // shift opt \ für \n neue Zeile in Console \t tab in Zeile
+
+}
+
+
+void counter () {
+  for (var i = 0; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+}
